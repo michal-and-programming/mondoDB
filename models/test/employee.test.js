f8f0b41ee8496473b802f8edf58d26f7dd125bca
Michał Puchała <michalpuchala86@gmail.com>
2025-10-30 20:28:41 +0100

    add test to employee.model.js

---
+++ models/test/employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require('../employee.model.js');
+const expect = require('chai').expect;
+const mongoose = require('mongoose');
+
+describe('Employee', () => {
+
+  it('should throw an error if no "firstName and lastName" arg', async () => {
+    const emp = new Employee({});
+  
+    emp.validateSync(err => {
+      expect(err.errors.firstName).to.exist;
+      expect(err.errors.lastName).to.exist;
+      expect(err.errors.department).to.exist;
+    });
+  });
+
+  it('should throw an error if "firstName and lastName" is not a string', () => {
+    const cases = [{}, []];
+    for(let name of cases) {
+       const emp = new Employee({ firstName: name, lastName: name });
+  
+      emp.validateSync(err => {
+        expect(err.errors.firstName).to.exist;
+        expect(err.errors.lastName).to.exist;
+      });
+    }
+  });
+
+  after(() => {
+    mongoose.models = {};
+  });
+});
